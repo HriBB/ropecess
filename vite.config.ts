34c933eb894513b0bb67613809d6e776f0f35746
Bojan Hribernik <bojan.hribernik@gmail.com>
2024-09-04 20:28:22 +0200

    Work in progress

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,36 @@ import { vitePlugin as remix } from '@remix-run/dev'
 import { installGlobals } from '@remix-run/node'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import minipic from 'vite-plugin-minipic'
 
 installGlobals({ nativeFetch: true })
 
 export default defineConfig({
   plugins: [
+    minipic({
+      sharpOptions: {
+        png: {
+          quality: 70,
+        },
+        jpg: {
+          quality: 70,
+        },
+        gif: {},
+        jpeg: {
+          quality: 70,
+        },
+        webp: {
+          quality: 70,
+        },
+        avif: {
+          quality: 70,
+        },
+      },
+      convert: [{ from: 'jpg', to: 'webp' }],
+      cache: false,
+      exclude: [],
+      include: [],
+    }),
     remix({
       future: {
         unstable_singleFetch: true,
